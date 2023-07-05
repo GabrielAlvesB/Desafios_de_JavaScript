7e2ec5ddbf9f2a0a1d2863054a7abb847657fc81
Gabriel Alves <75049066+GabrielAlvesB@users.noreply.github.com>
2023-07-05 20:49:54 -0300

    Inicio das atividades do modulo 2

---
+++ Modulo 2/Atividade 1/index.js
@@ -0,0 +1,11 @@
+const numero = 9
+
+function verificador(numero) {
+    if(numero % 2 == 0){
+        console.log(`Esse numero: ${numero} e par`)
+    }
+    else{
+        console.log(`Esse numero: ${numero} e impar`)
+    }
+}
+verificador(numero)
---
+++ Modulo 2/Atividade 2/index.js
@@ -0,0 +1,6 @@
+const fatorial = 5;
+let resultado = fatorial;
+for (let i = 1; i < fatorial; i++) {
+    resultado *= i;
+}
+console.log(resultado);
